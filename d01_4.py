910c52d8ccd19132ef61d6e8673f6b6b989b2377
양준영 <yangjunyeong229@gmail.com>
2023-01-16 17:53:17 +0900

    d01work

---
+++ d01_4.py
@@ -0,0 +1,14 @@
+n = 1
+result = 0
+for n in range(1,1000):
+    if n % 2 == 0 or n % 7 == 0:
+        result += n
+
+print(result)
+
+# 2와 7의 배수 전부  합하기 (1000미만)
+
+
+
+
+
---
+++ d01_5.py
@@ -0,0 +1,10 @@
+m = 5
+n = 4
+
+for a in range(m):
+    for q in range(n):
+        print('*',end = '')
+    print('')
+
+#가로의 길이가 n 세로의 길이가 m인 직사각형을 *로 출력하기
+
