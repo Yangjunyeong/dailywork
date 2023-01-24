effa3c0a900b9a7428dbc9e7e82815994fff78e0
yangjunyeong <yangjunyeong229@gmail.com>
2023-01-24 23:52:00 +0900

    1월 24일

---
+++ d02_05.py
@@ -0,0 +1,8 @@
+todo = [("Python Homework", 3), ("Assay", 4), ("Vacation", 100)]
+todowork = input()
+tododay = int(input())
+
+tuple_todo = (todowork, tododay)
+
+todo.append(tuple_todo)
+print(todo)
